2690ea9d2f658b0bfcd5260ca20648f3a12b26ce
Ayyah-Coded <gbolahanyahyah@gmail.com>
2026-06-12 15:52:32 +0100

    Implemented the Navbar and ThemeSelector

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router"
+import Navbar from "./components/Navbar"
 
 
 function App() {
   
   return (
-    <div>
-      <h1>Hello Vite</h1>
+    <div className="min-h-screen bg-base-100">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/edit/:id" element={<EditProductPage />} />
+        </Routes>
+      </main>
     </div>
   )
 }
