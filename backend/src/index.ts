a2fa55386ce97cd93d71e11dc51a8176eefffd81
Ayyah-Coded <gbolahanyahyah@gmail.com>
2026-06-12 12:23:50 +0100

    Add env variables and middleware setup for Express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
 
 import { ENV } from "./config/env";
 
 const app = express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 
