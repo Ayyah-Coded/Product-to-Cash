9318bd25c2b1f01190fdd12f74a982a5ef08e9bc
Ayyah-Coded <gbolahanyahyah@gmail.com>
2026-06-12 17:47:03 +0100

    - Refactor imports from @clerk/clerk-react to @clerk/react across components and hooks - Add package.json and package-lock.json

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-import { ClerkProvider } from "@clerk/clerk-react";
+import { ClerkProvider } from "@clerk/react";
 import { BrowserRouter } from 'react-router';
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
